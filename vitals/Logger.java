4087ab4082f0421dabb2a2c4c4313355156f3ebc
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-08-03 12:56:06 +0530

    build fix

--- vitals/Logger.java
+++ vitals/Logger.java
@@ -3,9 +3,9 @@ package vitals;
 public class Logger {
 	
 	static void printLogger(String category, IMessage type) {
-		
-		printMessage(type.getMessage(category));
-	}
+		if(!category.isEmpty())
+			printMessage(type.getMessage(category));
+	}	
 	
 	static void printMessage(String message) {
 		System.out.println(message);
